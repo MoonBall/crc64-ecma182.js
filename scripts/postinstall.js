eea8eb189a441e8b3fc6385cb6c93a57976cd6b0
Gang Chen <gangc.cxy@foxmail.com>
2024-03-04 18:38:50 +0800

    tos-crc64-js@0.0.1-alpha.2

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,6 +3,7 @@
 const cp = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const transform = require('./transform');
 
 if (!fs.existsSync(path.join(__dirname, '../dist/crc.js'))) {
   console.log('Compiling crc.js...');
@@ -10,4 +11,5 @@ if (!fs.existsSync(path.join(__dirname, '../dist/crc.js'))) {
   cp.execSync('make js', {
     cwd: path.join(__dirname, '..'),
   });
+  transform();
 }
