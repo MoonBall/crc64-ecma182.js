83f433a60dc9ff33d243790e179f7f7c5db05302
Gang Chen <gangc.cxy@foxmail.com>
2024-03-04 18:38:50 +0800

    tos-crc64-js@0.0.1-alpha.2

---
+++ babel-origin.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  retainLines: true,
+  compact: false,
+};
---
+++ babel-transform.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  retainLines: true,
+  compact: false,
+  plugins: [
+    '@babel/plugin-transform-optional-chaining',
+    '@babel/plugin-transform-logical-assignment-operators',
+  ],
+};
